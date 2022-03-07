180e9f867cd161f7e26f7f9d282224fd071be57f
Fhjorth <43441370+Fhjorth@users.noreply.github.com>
2022-03-07 20:54:22 +0100

    Resuable table

--- AngularTemplate/src/app/app.component.ts
+++ AngularTemplate/src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularTemplate';
+
+  totalCount = 0;
+
+  data1 = [ {
+    name: 'John Johnson',
+    dept: 'Electrical'
+  }, {
+    name: 'Jens Jensen',
+    dept: 'Mechanical'
+  }, {
+    name : 'Jim Lasker',
+    dept : 'Medical'
+  }];
+
+  data2 = [ {
+    name: 'John Johnson',
+    dept: 'Electrical'
+  }, {
+    name: 'Jens Jensen',
+    dept: 'Mechanical'
+  }, {
+    name : 'Jim Lasker',
+    dept : 'Medical'
+  }];
+
+  calCount(count){
+    this.totalCount = this.totalCount + count;
+  }
 }
---
+++ AngularTemplate/src/app/data-list/data-list.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-data-list',
+  templateUrl: './data-list.component.html',
+  styleUrls: ['./data-list.component.css']
+})
+export class DataListComponent implements OnInit {
+
+  @Input() data;
+  @Input() showCount = false;
+  @Output() calCount = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.calCount.emit(this.data.length);
+  }
+
+}
